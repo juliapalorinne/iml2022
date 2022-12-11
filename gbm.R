da80bdcec12db61060e43bae1b29cf9788f05175
Julia Palorinne <julia.palorinne@helsinki.fi>
2022-12-11 23:45:25 +0200

    gbm

--- gbm.R
+++ gbm.R
@@ -1,41 +1,55 @@
 library(gbm) # GBM
 set.seed(1)
-
 npfOrig <- read.csv("npf_train.csv")
-
-# Create class2, 0 = nonevent, 1 = event
 npfOrig$class2 <- ifelse(npfOrig$class4 == "nonevent", 0, 1)
-
-# Add date as row name
 rownames(npfOrig) <- npfOrig[, "date"]
-
-# Select global and 16.8 m high values
 npf.168 <- data.frame(npfOrig[, (5:6)], npfOrig[, (13:16)], npfOrig[, (27:30)], 
                       npfOrig[, (41:42)], npfOrig[, (53:54)], npfOrig[, (63:72)],
                       npfOrig[, (83:88)], npfOrig[, (99:105)])
 
-# Remove columns id, date, class4, partlybad from npf
-npf <- npfOrig[, -(1:4)]
+npfsp <- npf.168[, c(1:2, 5:6, 9:10, 13:16, 19:20, 23:24, 27:30, 37)]
+colnames(npfsp)
+npfsp.fact <- npfsp
+npfsp.fact[, 19] <- as.factor(npfsp.fact[, 19])
 
-# Create datasets where class2 is a factor
-npf.fact <- npf
-npf.fact$class2 <- factor(npf.fact$class2)
-npf.168.fact <- npf.168
-npf.168.fact$class2 <- as.factor(npf.168.fact$class2)
+npf4<-npfOrig[,-c(1,2,4)]
+npf4.168 <- data.frame(npf4[, (1:3)], npf4[, (10:13)], npf4[, (24:27)], 
+                       npf4[, (38:39)], npf4[, (50:51)], npf4[, (60:69)],
+                       npf4[, (80:85)], npf4[, (96:101)])
+npf4sp<-npf4.168[,c(1,2:3,6:7,10:11,14:17,20:21,24:25,28:31)]
+
+idx <- sample(nrow(npfsp.fact), nrow(npfsp.fact)/2)
+npf_train <- npfsp[idx, ]
+npf_test <- npfsp[-idx, ]
+
+gbm.mod <- gbm(class2 ~ ., data=npf_train, distribution = "bernoulli", n.trees = 5000)
+gbm.pred <- predict(gbm.mod, newdata=npf_test, n.trees = 100, type = "response")
+
+gbm.pred
+
+n = nrow(npfsp)
+npf.gbm.LOOCV <- rep(0, n)
+
+# loocv
+for (i in 1:dim(npfsp)[1]) {
+  mod <- gbm(class2 ~ ., data=npf_train, distribution = "bernoulli", n.trees = 5000)
+  npf.gbm.LOOCV[i] <- predict(mod, npfsp[i, ], n.trees = 5000, type = "response")
+  print(i)
+}
+
+accuracies <- data.frame(original = accuracy(gbm.pred, npf_test$class2),
+                         LOOCV = accuracy(npf.gbm.LOOCV, npfsp$class2))
+perplexities <- data.frame(original = perplexity(gbm.pred, npf_test$class2),
+                           LOOCV = perplexity(npf.gbm.LOOCV, npfsp$class2))
+table <- cbind(rbind(accuracies), rbind(perplexities))
+
+knitr::kable(table, digits = 3) %>%
+  add_header_above(c("Accuracy" = 2, "Perplexity" = 2)) %>%
+  kable_styling(latex_options = "HOLD_position")
 
 
-# Select training and test data
-idx <- sample(nrow(npf.168), nrow(npf.168)/2)
-npf_train168 <- npf.168[idx, ]
-npf_test168 <- npf.168[-idx, ]
 
-boost.npf <- gbm(class2~., data=npf_train168, 
-                        distribution="bernoulli", n.trees=5000)
-summary(boost.npf)
-# plot(boost.npf, i=c("RHIRGA168.mean"))
-# plot(boost.npf, i=c("RHIRGA168.mean", "H2O168.mean"))
 
-gbm.pred <- predict(boost.npf, newdata=npf_test168, n.trees=5000, type="response")
 
 accuracy <- function(pred, y) mean(ifelse(pred >= 0.5, 1, 0) == y)
 accuracy(gbm.pred, npf_test$class2)
